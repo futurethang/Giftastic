f1e206753dc8689c2a23dad1824f5bd082acdc23
Kevin <kphyde@gmail.com>
2018-09-14 14:37:22 -0700

    add html divs to contain AJAX returns, javascript to build buttons and populate gifs in still form from response object, includes data-still and data-animate for click actions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,42 @@
 var topics = [
-    "climate change",
+    "Climate Change",
     "ALF",
     "Star Trek",
     "Country Music",
-    "books",
+    "Books",
     "Buffalo Wings",
     "Adventure Time",
 ]
 
+// var $buttonModel = ("<button class='btn' data-topic='" + searchString + "' id='" + id + "'></button>");
+
+function buttonModel(arrItem) { // HTML MARKUP FOR BUTTON DOM ELEMENTS
+    return "<button class='btn' data-search='" + arrItem + "'>" + arrItem + "</button>"
+}
+
+// CONSTRUCT THE BUTTONS FROM TOPICS ARRAY
+for (let i = 0; i < topics.length; i++) {
+    $("#button-area").append(buttonModel(topics[i]))
+}
+
+$(".btn").on("click", function() {
+    var searchTerm = $(this).attr("data-search");
+    console.log(searchTerm);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=eG2Lv5jBWCBc6paULljFPRmKiTrwDLzI&limit=10";
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=ALF&limit=10&api_key=&eG2Lv5jBWCBc6paULljFPRmKiTrwDLzI";
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response) {
+        var results = response.data;
+        console.log(results);
+        
+        for (let i = 0; i < results.length; i++) { // LOOP TO OUTPUT 10 GIFS
+            var still = results[i].images.original_still.url;
+            var animated = results[i].images.downsized.url;
+            var img = "<img src='"+ results[i].images.original_still.url +"' data-still="+ still + " data-animated="+ animated +" />";
+            $(".main-body").append(img);            
+        }
+    })
+});

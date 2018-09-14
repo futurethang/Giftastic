2d604ec96d5eb64da104fca64fceeb23aea78b40
Kevin <kphyde@gmail.com>
2018-09-14 15:02:36 -0700

    success in making the gifs animate when clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,6 +19,20 @@ for (let i = 0; i < topics.length; i++) {
     $("#button-area").append(buttonModel(topics[i]))
 }
 
+function addAnimateListener() {
+    $(document).on("click", ".gif", function() {
+        console.log("triggered");
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("data-state", "animated");
+          } else if (state === "animated") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+    })
+}
+
 $(".btn").on("click", function() {
     var searchTerm = $(this).attr("data-search");
     console.log(searchTerm);
@@ -35,8 +49,14 @@ $(".btn").on("click", function() {
         for (let i = 0; i < results.length; i++) { // LOOP TO OUTPUT 10 GIFS
             var still = results[i].images.original_still.url;
             var animated = results[i].images.downsized.url;
-            var img = "<img src='"+ results[i].images.original_still.url +"' data-still="+ still + " data-animated="+ animated +" />";
+            var img = "<img src='"+ results[i].images.original_still.url +"' data-still="+ still + " data-animated="+ animated +" data-state='still' class='gif'/>";
             $(".main-body").append(img);            
         }
     })
+    addAnimateListener();
 });
+
+
+
+
+
